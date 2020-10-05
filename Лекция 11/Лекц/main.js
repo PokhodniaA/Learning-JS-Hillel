6fb64a825e49bf76fab303f0d3328d36effbcdc2
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-10-05 19:18:47 +0300

    Add lect 11

--- Лекция 11/Лекц/main.js
+++ Лекция 11/Лекц/main.js
@@ -5,3 +5,48 @@
 // 2. staged
 // 3. modified
 // 4. commited
+
+
+
+
+
+
+
+
+
+
+// Сделать буквы заглавными
+// String.prototype.toJadenCase = function () {
+//     return this.split(' ').map(function (words) {
+//         return words[0].toUpperCase() + words.slice(1)
+//     }).join(' ')
+// };
+
+// var str = "How can mirrors be real if our eyes aren't real";
+// console.log(str.toJadenCase());
+
+
+// Найти квадрат
+// var isSquare = function(n){
+//   return (Number.isInteger(Math.sqrt(n))) ? true : false // fix me
+// }
+
+// console.log(isSquare(26));
+// console.log(isSquare(25));
+
+
+// Пирамида не четных чисел // херня
+// function rowSumOddNumbers(n) {
+//     var cnt = 1;
+//     for(i = 1; i < n; i++){
+//         for(j = 0; j < i; j++){
+//             cnt += 2;
+//         }
+//     }
+
+//     cnt = (cnt * (n + 1)) - 1;
+//     return cnt
+// }
+
+// console.log(rowSumOddNumbers(1));
+// console.log(rowSumOddNumbers(42));
