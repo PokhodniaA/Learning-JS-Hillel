ead7d0df0bcd243fed7b8d8531ba507633525d37
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-10-10 15:50:39 +0300

    All added

--- Лекция 12/Homework 12/EX.3/main.js
+++ Лекция 12/Homework 12/EX.3/main.js
@@ -4,7 +4,5 @@ function func(surname, patronymic) {
     alert('привет' + ', ' + this.name + ' ' + patronymic + ' ' + surname);
 }
 
-//Тут напишите конструкцию с bind()
 func = func.bind(user);
-
-func('Николаев', 'Иванович'); //тут должно вывести 'привет, Алексей Иванович Николаев 's
+func('Николаев', 'Иванович');
