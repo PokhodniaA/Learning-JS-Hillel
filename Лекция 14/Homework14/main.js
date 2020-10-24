edc3d43b347829baf0b7f53b07bd31db0aaeb678
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-10-24 12:00:26 +0300

    Added lect 14

---
+++ Лекция 14/Homework14/main.js
@@ -0,0 +1,35 @@
+function SuperMath() {
+    this.check = function (obj) {
+        var x = obj.x, y = obj.y, znak = obj.znak;
+        var answer = confirm(`Вы хотите ${x} ${znak} ${y} ?`);
+        (answer) ? document.write(this.math(x, y, znak)) : this.input()
+    }
+}
+
+SuperMath.prototype.input = function () {
+    var input = prompt('Введите x знак y ', '');
+    if (input) {
+        var arrInput = input.split(' ');
+        var x = +arrInput[0], y = +arrInput[2], znak = arrInput[1];
+        document.write(this.math(x, y, znak));
+    } else {
+        document.write('Отменено');
+    }
+}
+
+SuperMath.prototype.math = function (x, y, znak) {
+    switch (znak) {
+        case '+': return x + y;
+        case '-': return x - y;
+        case '*': return x * y;
+        case '/': return x / y;
+        case '%': return x % y;
+        default: return 'Такого математического действия нет.';
+    }
+}
+
+var obj = { 'x': 12, 'y': 3, 'znak': '/' };
+var p = new SuperMath();
+
+p.check(obj)
+
