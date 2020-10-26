38dde1d524dfeb38e702019fb46a669816f4c4e3
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-10-26 13:37:49 +0200

    Fixed HW14

--- Лекция 14/Homework14/main.js
+++ Лекция 14/Homework14/main.js
@@ -5,25 +5,29 @@ function SuperMath() {
         (answer) ? document.write(this.math(x, y, znak)) : this.input()
     }
 
-    SuperMath.prototype.input = function () {
-        var input = prompt('Введите x знак y ', '');
-        if (input) {
-            var arrInput = input.split(' ');
-            var x = +arrInput[0], y = +arrInput[2], znak = arrInput[1];
-            document.write(this.math(x, y, znak));
-        } else {
-            document.write('Отменено');
+    if (!SuperMath.prototype.input) {
+        SuperMath.prototype.input = function () {
+            var input = prompt('Введите x знак y ', '');
+            if (input) {
+                var arrInput = input.split(' ');
+                var x = +arrInput[0], y = +arrInput[2], znak = arrInput[1];
+                document.write(this.math(x, y, znak));
+            } else {
+                document.write('Отменено');
+            }
         }
     }
 
-    SuperMath.prototype.math = function (x, y, znak) {
-        switch (znak) {
-            case '+': return x + y;
-            case '-': return x - y;
-            case '*': return x * y;
-            case '/': return x / y;
-            case '%': return x % y;
-            default: return 'Такого математического действия нет.';
+    if (!SuperMath.prototype.math) {
+        SuperMath.prototype.math = function (x, y, znak) {
+            switch (znak) {
+                case '+': return x + y;
+                case '-': return x - y;
+                case '*': return x * y;
+                case '/': return x / y;
+                case '%': return x % y;
+                default: return 'Такого математического действия нет.';
+            }
         }
     }
 }
