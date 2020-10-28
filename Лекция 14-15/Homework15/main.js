d4f8e6ab45184a7f77db4f3c026a28494decf833
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-10-28 22:03:13 +0200

    HW15 V0.1

--- Лекция 14-15/Homework15/main.js
+++ Лекция 14-15/Homework15/main.js
@@ -1,35 +1,104 @@
-function SuperMath() {
-    this.check = function (obj) {
-        var x = obj.x, y = obj.y, znak = obj.znak;
-        var answer = confirm(`Вы хотите ${x} ${znak} ${y} ?`);
-        (answer) ? document.write(this.math(x, y, znak)) : SuperMath.input()
+// Осталось: добавить методы в прототип;
+// сделать рефаторинг;
+// ---------------Field-----------------
+
+function Field(width, height) {
+    this.grid = Field.fillGrid(width, height);
+
+    this.renderField = function () {
+        document.write('<hr>');
+        for (var i = 0; i < this.grid.length; i++) {
+            document.write(this.grid[i], '<br>')
+        }
+        document.write('<hr>');
+    }
+
+    this.clearField = function () { //(*2*)
+        this.grid.forEach(function (item, index, array) {
+            var element = item.indexOf(1);
+            if (element >= 0) { array[index][element] = 0 }
+        })
+    }
+
+    this.changeSize = function (newX, newY) {
+        this.grid = Field.fillGrid(newX, newY);
     }
 }
 
-SuperMath.input = function () {
-    var input = prompt('Введите x знак y ', '');
-    if (input) {
-        var arrInput = input.split(' ');
-        var x = +arrInput[0], y = +arrInput[2], znak = arrInput[1];
-        document.write(SuperMath.prototype.math(x, y, znak));
-    } else {
-        document.write('Отменено');
+Field.fillGrid = function (width, height) { // перенести fillgrid в конструктор ?
+    var array = [];
+    for (var i = 0; i < height; i++) {
+        var tempWidthArray = [];
+        for (var j = 0; j < width; j++) {
+            tempWidthArray.push(0);
+        }
+        array.push(tempWidthArray);
+    }
+    return array
+}
+
+// ------------Person----------------
+
+function Person(name, XPosition, YPosition) {
+    this.name = name;
+    this.positionX = XPosition, this.positionY = YPosition;
+    this.defaultX = XPosition, this.defaultY = YPosition;
+
+    this.start = function () {
+        this.grid[this.positionY][this.positionX] = 1;
+    }
+
+    this.go = function go(direction, step) {
+        this.grid[this.positionY][this.positionX] = 0;
+        switch (direction) {
+            case 'left':
+                this.positionX -= step;
+                break;
+            case 'right':
+                this.positionX += step;
+                break;
+            case 'top':
+                this.positionY -= step
+                break;
+            case 'bottom':
+                this.positionY += step;
+                break;
+        }
+
+        this.start();
+    }
+
+    this.resetPosition = function () {
+        this.grid[this.positionY][this.positionX] = 0;
+        this.grid[this.defaultY][this.defaultX] = 1;
     }
 }
 
-SuperMath.prototype.math = function (x, y, znak) {
-    switch (znak) {
-        case '+': return x + y;
-        case '-': return x - y;
-        case '*': return x * y;
-        case '/': return x / y;
-        case '%': return x % y;
-        default: return 'Такого математического действия нет.';
-    }
-}
 
-var obj = { 'x': 12, 'y': 3, 'znak': '/' };
-var p = new SuperMath();
+// -------------Main---------------
 
-p.check(obj)
+var field = new Field(10, 10);
+console.log(field, 'field');
+Person.prototype = Object.create(field); //(*1*)
+var person = new Person('Andrey', 5, 4);
 
+console.log(person);
+
+field.renderField();
+person.start();
+field.renderField();
+person.go('top', 2);
+person.go('left', 3);
+field.renderField();
+person.resetPosition();
+field.renderField();
+field.clearField();
+field.renderField();
+
+
+
+// -------------Вопросы------------
+
+// 1. Правильное ли наследование Person.protorype от field или лучше передавать объект в метод(*1*)
+// 2. Можно ли как-то получить значения позици Х и У person (*2*)
+// 
