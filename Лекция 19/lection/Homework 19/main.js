5a89441e5e6a137c0978fccb830f58a1afe2c6ac
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-11-13 13:20:39 +0200

    HW19 Batman edition v1.0

--- Лекция 19/lection/Homework 19/main.js
+++ Лекция 19/lection/Homework 19/main.js
@@ -1,48 +1,90 @@
+function getPositionX(coordinateX) {
+    let windowWidth = window.innerWidth;
+    if (coordinateX < 0) {
+        return 0
+    } else if (coordinateX > windowWidth - defaultWidth) {
+        return windowWidth - defaultWidth;
+    } else {
+        return coordinateX
+    }
+}
+
+function getPositionY(coordinateY) {
+    let windowHeight = window.innerHeight;
+    if (coordinateY < defaultHeight) {
+        return defaultHeight;
+    } else if (coordinateY > windowHeight) {
+        return windowHeight;
+    } else {
+        return coordinateY
+    }
+}
+
 function moveXAxis(event) {
-    let personX = parseInt(computedStylePerson.left);
     switch (event.code) {
         case 'ArrowRight':
-            person.style.left = personX + step + 'px';
+            person.style.left = getPositionX(parseInt(computedStylePerson.left) + step) + 'px';
             break
         case 'ArrowLeft':
-            person.style.left = personX - step + 'px';
+            person.style.left = getPositionX(parseInt(computedStylePerson.left) - step) + 'px';
             break
+        default:
+            break;
     }
 }
 
 function moveYAxis(event) {
-    let personY = parseInt(computedStylePerson.top);
     switch (event.code) {
-        case 'Space':
+        case 'ArrowUp':
+            person.style.top = getPositionY(parseInt(computedStylePerson.top) - step) + 'px';
+            break
+        case 'ArrowDown':
+            person.style.top = getPositionY(parseInt(computedStylePerson.top) + step) + 'px';
+            break
+        case 'Space': // (*FIXED*)Bug: При многократном нажатии на пробел меня позици Y.
             if (event.repeat == false) {
+                let personY = parseInt(computedStylePerson.top);
                 person.style.top = personY - jumpHeight + 'px';
-                setTimeout(() => { person.style.top = personY + 'px'; }, 500);
+                body.onkeydown = moveXAxis
+                setTimeout(() => {
+                    person.style.top = personY + 'px';
+                    setTimeout(() => {
+                        body.onkeydown = keyDown;
+                    }, 400);
+                }, 300);
             }
             break;
         case 'KeyZ':
-            person.style.width = defaultWidth * 1.15 + 'px';
-            person.style.height = defaultHeight * 0.6 + 'px';
+            // person.style.width = defaultWidth * 1.15 + 'px';
+            // person.style.height = defaultHeight * 0.6 + 'px';
+            person.setAttribute('src', 'img/batman_seat.png');
             body.onkeydown = moveXAxis;
+            break;
+        default:
             break;
     }
 }
 
 function keyDown(event) {
     moveXAxis(event);
-    moveYAxis(event)
+    moveYAxis(event);
+    console.log(event.code);
 }
 
 function keyUp(event) {
     switch (event.code) {
         case 'KeyZ':
-            person.style.width = defaultWidth + 'px';
-            person.style.height = defaultHeight + 'px';
+            // person.style.width = defaultWidth + 'px';
+            // person.style.height = defaultHeight + 'px';
+            person.setAttribute('src', 'img/batman.png');
             body.onkeydown = keyDown;
             break;
         default:
             break;
     }
 }
+
+
 let person = document.querySelector('#person'),
     body = document.querySelector('body'),
     computedStylePerson = getComputedStyle(person),
@@ -55,3 +97,10 @@ let person = document.querySelector('#person'),
 
 body.onkeydown = keyDown;
 body.onkeyup = keyUp;
+
+
+// Cписок задач:
+
+// 1.Пофиксить прыжок(!Done!)
+// 2. Ограничить размеры экрана(!Done!)
+// 3 переделать при помощи классов
