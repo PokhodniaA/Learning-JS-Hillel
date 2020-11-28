2005a266fb25377f8ce5710e1d7b8ae6582eb3c5
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-11-28 21:55:50 +0200

    HW24

--- Лекция 24/Homework24/client/main.js
+++ Лекция 24/Homework24/client/main.js
@@ -1,3 +1,49 @@
+function Form() {
+    this.nodes = {
+        form: document.createElement('form'),
+        button: document.createElement('button'),
+        loginLabel: document.createElement('label'),
+        login: document.createElement('input'),
+        passwordLabel: document.createElement('label'),
+        password: document.createElement('input'),
+    }
+}
+
+Form.prototype.toDo = function () {
+    this.appendToNodes();
+    this.build();
+    return this
+}
+
+
+Form.prototype.appendToNodes = function () {
+    const { form, button, loginLabel, login, passwordLabel, password } = this.nodes;
+    form.classList.add('right');
+    loginLabel.textContent = 'Login:';
+    login.name = 'login';
+    passwordLabel.textContent = 'Password:'
+    password.name = 'password';
+    button.textContent = 'Sign in';
+}
+
+Form.prototype.build = function () {
+    const { form, button, loginLabel, login, passwordLabel, password } = this.nodes;
+    loginLabel.append(login);
+    passwordLabel.append(password);
+    form.append(loginLabel);
+    form.append(passwordLabel);
+    form.append(button);
+}
+
+Form.prototype.render = function () {
+    document.body.append(this.nodes.form);
+    setTimeout(() => {
+        this.nodes.form.classList.remove('right');
+        this.nodes.form.classList.add('center')
+    }, 100)
+
+}
+
 // ======================================
 function ErrorBlock() {
     this.block = document.createElement('div');
@@ -20,39 +66,19 @@ ErrorBlock.prototype.show = function () {
 }
 // ======================================
 
-function getStart(form) {
-    form.classList.remove('right');
-    form.classList.add('center')
-}
-
-
-function getForm() {
-    const form = document.querySelector('form'),
-        button = form.querySelector('button'),
-        login = form.querySelector('input[name=login]'),
-        password = form.querySelector('input[name=password]');
-    return {
-        form: form,
-        button: button,
-        login: login,
-        password: password,
-    }
-}
-
 window.onload = function () {
-    const signInForm = getForm(),
-        errorEmptyData = new ErrorBlock();
+    const signInForm = new Form(),
+        form = signInForm.nodes.form;
+    errorEmptyData = new ErrorBlock();
 
+    signInForm.toDo().render();
     errorEmptyData.addToNode('You have not entered data').render();
-    console.log(signInForm);
-    getStart(signInForm.form)
 
-    signInForm.form.onsubmit = function (event) {
+    form.onsubmit = function (event) {
         event.preventDefault();
-        let login = signInForm.login.value,
-            password = signInForm.password.value;
+        let login = form.login.value,
+            password = form.password.value;
 
         (login || password) ? console.log(login, password) : errorEmptyData.show();
     };
 }
-
