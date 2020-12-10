146f1c3c3332726e6d483b8ab99e064a3975102d
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-12-10 09:01:21 +0200

    HW27 V0.2.0

--- Лекция 26-27/Homework27/main.js
+++ Лекция 26-27/Homework27/main.js
@@ -1,16 +1,18 @@
 Vue.component('diagrams', {
     data() {
         return {
-            listOfDiagrams: listOfDiagrams,
+            listOfDiagrams: [],
         }
     },
     methods: {
         sortDiagrams() {
-            this.listOfDiagrams.sort((a, b) => {
-                return (a.diagramHeight > b.diagramHeight) ? 1 : -1;
-            });
-            console.log(this.listOfDiagrams);
-        }
+            this.listOfDiagrams.sort((a, b) => (a.diagramHeight > b.diagramHeight) ? 1 : -1);
+            this.setToLocalStorage();
+        },
+        setToLocalStorage() {
+            const JSONlistOfDiagrams = JSON.stringify(this.listOfDiagrams);
+            localStorage.setItem('listOfDiagrams', JSONlistOfDiagrams);
+        },
     },
     template: `
         <div>
@@ -18,6 +20,7 @@ Vue.component('diagrams', {
                 <diagram
                     v-for="item in listOfDiagrams"
                     :sendedDiagram="item"
+                    @toLocalStorage="setToLocalStorage"
                 />
             </div>
 
@@ -27,58 +30,66 @@ Vue.component('diagrams', {
                 @click="sortDiagrams"
             >Sort</button>
         </div>
-    `
+    `,
+    created() {
+        const item = localStorage.getItem('listOfDiagrams');
+        console.log(this.listOfDiagrams);
+        this.listOfDiagrams = (item) ? JSON.parse(item) : listOfDiagrams
+    }
+    // через хук брать данные из localstorage если есть.
 });
 
 Vue.component('diagram', {
     props: ['sendedDiagram'],
     data() {
         return {
-            rangeValue: this.getFormLocalStorage(),
+            // rangeValue: this.getFormLocalStorage(),
         }
     },
     methods: {
-        setToLocalStorage(event, value = this.rangeValue) {
-            console.log(value);
-            localStorage.setItem(this.sendedDiagram.diagramName, value)
-        },
-        getFormLocalStorage() {
-            const storageValue = localStorage.getItem(this.sendedDiagram.diagramName);
-            let value = storageValue;
-            if (!storageValue) {
-                value = this.getHeightColumn();
-                this.setToLocalStorage(null, value)
-            }
-            return value
-        },
-        getHeightColumn() {
-            const diagramHeight = this.sendedDiagram.diagramHeight;
-            return (diagramHeight) ? diagramHeight : 100;
+        toLocalStorage() {
+            this.$emit('toLocalStorage');
         }
+        // setToLocalStorage(event, value = this.rangeValue) {
+        //     console.log(value);
+        //     localStorage.setItem(this.sendedDiagram.diagramName, value)
+        // },
+        // getFormLocalStorage() {
+        //     const storageValue = localStorage.getItem(this.sendedDiagram.diagramName);
+        //     let value = storageValue;
+        //     if (!storageValue) {
+        //         value = this.getHeightColumn();
+        //         this.setToLocalStorage(null, value)
+        //     }
+        //     return value
+        // },
     },
     computed: {
         colorColumn() {
             const diagramColor = this.sendedDiagram.diagramColor;
             return (diagramColor) ? diagramColor : 'black';
         },
-
+        getHeightColumn() {
+            const diagramHeight = this.sendedDiagram.diagramHeight;
+            return (diagramHeight) ? diagramHeight : 100;
+        },
     },
     template: `
         <div class='diagram'>
             <div 
                 class="column"
                 :class="colorColumn"
-                :style="{height: rangeValue + 'px'}"
+                :style="{height: getHeightColumn + 'px'}"
             ></div>
             <input 
                 type="range" 
                 min="100"
                 max="300"
-                v-model="rangeValue"
-                @change="setToLocalStorage"
+                v-model="sendedDiagram.diagramHeight"
+                @change="toLocalStorage"
             >
         </div>
-`
+` // событе @change добавлять в localstor  sendedDiagram. Возможно передать событие на родительский блок, а он уже будет добавлять весь массив.
 });
 
 // Vue.component('column', {
