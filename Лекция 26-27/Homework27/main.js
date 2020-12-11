9d18c66d5fe78cd22747d7ac0c36f11357236556
Pokhodnia Andrii <pokhodnia.a@gmail.com>
2020-12-11 09:33:02 +0200

    HW27 V1.0.0

--- Лекция 26-27/Homework27/main.js
+++ Лекция 26-27/Homework27/main.js
@@ -10,8 +10,7 @@ Vue.component('diagrams', {
             this.setToLocalStorage();
         },
         setToLocalStorage() {
-            const JSONlistOfDiagrams = JSON.stringify(this.listOfDiagrams);
-            localStorage.setItem('listOfDiagrams', JSONlistOfDiagrams);
+            localStorage.setItem('listOfDiagrams', JSON.stringify(this.listOfDiagrams));
         },
     },
     template: `
@@ -27,42 +26,23 @@ Vue.component('diagrams', {
             <hr />
 
             <button
+                class="sort_btn"
                 @click="sortDiagrams"
             >Sort</button>
         </div>
     `,
     created() {
         const item = localStorage.getItem('listOfDiagrams');
-        console.log(this.listOfDiagrams);
-        this.listOfDiagrams = (item) ? JSON.parse(item) : listOfDiagrams
+        this.listOfDiagrams = (item) ? JSON.parse(item) : listOfDiagrams;
     }
-    // через хук брать данные из localstorage если есть.
 });
 
 Vue.component('diagram', {
     props: ['sendedDiagram'],
-    data() {
-        return {
-            // rangeValue: this.getFormLocalStorage(),
-        }
-    },
     methods: {
         toLocalStorage() {
             this.$emit('toLocalStorage');
         }
-        // setToLocalStorage(event, value = this.rangeValue) {
-        //     console.log(value);
-        //     localStorage.setItem(this.sendedDiagram.diagramName, value)
-        // },
-        // getFormLocalStorage() {
-        //     const storageValue = localStorage.getItem(this.sendedDiagram.diagramName);
-        //     let value = storageValue;
-        //     if (!storageValue) {
-        //         value = this.getHeightColumn();
-        //         this.setToLocalStorage(null, value)
-        //     }
-        //     return value
-        // },
     },
     computed: {
         colorColumn() {
@@ -89,14 +69,8 @@ Vue.component('diagram', {
                 @change="toLocalStorage"
             >
         </div>
-` // событе @change добавлять в localstor  sendedDiagram. Возможно передать событие на родительский блок, а он уже будет добавлять весь массив.
+    `
 });
-
-// Vue.component('column', {
-//     template: `
-//         <div class="column"></div> 
-//     `
-// })
 
 const app = new Vue({
     el: "#app",
